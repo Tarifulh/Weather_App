361c6c6c4871df8ec4571837fb14aecef47a08fe
Tariful <tarifulhoque99@gmail.com>
2026-08-29 11:17:20 +0530

    Add Enter key for search

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,15 +1,19 @@
-let cityInput = document.getElementById("cityInput")
-let searchButton = document.getElementById("searchButton")
+let cityInput = document.getElementById("cityInput");
+let searchButton = document.getElementById("searchButton");
 
-searchButton.addEventListener("click", function() {
-    let city = cityInput.value
-    let url = "http://127.0.0.1:5000/weather?city=" + encodeURIComponent(city);
+
+function searchWeather() {
+
+    let city = cityInput.value;
+
+    let url = "http://127.0.0.1:5000/weather?city=" + encodeURI(city);
 
     fetch(url)
         .then(response => {
-            return response.json()
+            return response.json();
         })
         .then(data => {
+
             document.getElementById("temperature").textContent = data.temperature;
             document.getElementById("humidity").textContent = data.humidity;
             document.getElementById("wind").textContent = data.wind_speed;
@@ -18,5 +22,20 @@ searchButton.addEventListener("click", function() {
             document.getElementById("precipitations").textContent = data.precipitation;
             document.getElementById("snowfalls").textContent = data.snowfall;
             document.getElementById("conditions").textContent = data.condition;
-        })
+
+        });
+}
+
+
+// When Search button is clicked
+searchButton.addEventListener("click", searchWeather);
+
+
+// When Enter is pressed inside the input box
+cityInput.addEventListener("keydown", function(event) {
+
+    if (event.key === "Enter") {
+        searchWeather();
+    }
+
 });
